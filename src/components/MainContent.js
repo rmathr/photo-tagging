af2296e5ffc178cf7618395ed7214e00509acea8
Matheus <85845749+rmathr@users.noreply.github.com>
2023-06-29 17:05:22 -0400

    implement character search logic

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -11,18 +11,53 @@ const characters = [
     xCoord: 1144,
     yCoord: 7380,
   },
+  {
+    name: 'Brian',
+    found: false,
+    xCoord: 275,
+    yCoord: 5800,
+  },
+  {
+    name: 'Ash',
+    found: false,
+    xCoord: 54,
+    yCoord: 7060,
+  },
+  {
+    name: 'Link',
+    found: false,
+    xCoord: 450,
+    yCoord: 7734,
+  },
+  {
+    name: 'Wilson',
+    found: false,
+    xCoord: 1610,
+    yCoord: 8007,
+  },
 ];
 
 const MainContent = () => {
-  const [circle, setCircle] = useState([]);
+  const screenWidth = window.innerWidth;
+  const screenHeight = screen.height;
+  const radius = (0.032552 * screenWidth) / 2;
 
-  //   const getImageSize = () => {
-  //     const image = document.getElementById('image');
-  //     console.log(image.clientWidth, image.clientHeight);
-  //   };
-  //   useEffect(() => {
-  //     getImageSize();
-  //   }, []);
+  let characterCircle = (
+    <div
+      className={`absolute rounded-full opacity-50 bg-green-700 z-10`}
+      // style={{ top: y + 25, left: x - 25 }}
+      style={{
+        width: 2 * radius,
+        height: 2 * radius,
+        top: characters[0].yCoord * (screenWidth / 1920) + 0.07 * screenHeight - radius,
+        //   top: 7380 * (screenWidth / 1920) + 0.07 * screenHeight - radius,
+        left: characters[0].xCoord * (screenWidth / 1920) - radius,
+        //   left: 1144 * (screenWidth / 1920) - radius,
+      }}
+    ></div>
+  );
+
+  const [circle, setCircle] = useState([]);
 
   const getMouseCoords = (e) => {
     const height = screen.height;
@@ -52,6 +87,7 @@ const MainContent = () => {
       <div
         className={`absolute rounded-full opacity-50 bg-red-600 z-10`}
         // style={{ top: y + 25, left: x - 25 }}
+        onClick={(e) => handleClick(e)}
         style={{
           width: 2 * radius,
           height: 2 * radius,
@@ -65,17 +101,25 @@ const MainContent = () => {
     setCircle(newCircle);
     // console.log(x, y);
     return {
-      center: [x, y],
-      radius: 10,
+      center: [(x + radius) / (screenWidth / 1920), (y - radius) / (screenWidth / 1920)],
+      radius: 50,
     };
   };
 
   const handleClick = (e) => {
+    const screenWidth = window.innerWidth;
+    const screenHeight = screen.height;
+    const radius = (0.032552 * screenWidth) / 2;
     const clickedCircle = addCircle(e);
+    const circle2 = {
+      center: [characters[2].xCoord, characters[2].yCoord - radius],
+      radius: 1,
+    };
 
     console.log(clickedCircle);
-    const width = window.innerWidth;
-    console.log(width);
+    console.log(checkIfContains(clickedCircle, circle2));
+    // const width = window.innerWidth;
+    // console.log(width);
     // alert(checkIfContains(circle1, circle2));
   };
 
@@ -107,6 +151,8 @@ const MainContent = () => {
   return (
     <div className="w-full">
       {circle}
+      {/* {characterCircle} */}
+
       <img
         src={background2}
         id="image"
