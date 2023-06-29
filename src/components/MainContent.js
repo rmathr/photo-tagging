9bb29b4aa003e0e5cc7ac92c3dc8a1c188fa6e6b
Matheus <85845749+rmathr@users.noreply.github.com>
2023-06-29 11:55:40 -0400

    implement circle display when clicked on image

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,47 +1,82 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ImageMapper from 'react-img-mapper';
 
 import background from './assets/images/background.jpg';
 import background2 from './assets/images/background2.jpg';
 
+const characters = [
+  {
+    name: 'Edward Elric',
+    found: false,
+    xCoord: 1144,
+    yCoord: 7380,
+  },
+];
+
 const MainContent = () => {
   const [circle, setCircle] = useState([]);
+
+  //   const getImageSize = () => {
+  //     const image = document.getElementById('image');
+  //     console.log(image.clientWidth, image.clientHeight);
+  //   };
+  //   useEffect(() => {
+  //     getImageSize();
+  //   }, []);
+
+  const getMouseCoords = (e) => {
+    const height = screen.height;
+    let x = e.clientX;
+    let y = e.clientY;
+    console.log(x, y);
+    console.log(height);
+  };
 
   const getClickCoords = (event) => {
     // from: https://stackoverflow.com/a/29296049/14198287
     let e = event.target;
     let dim = e.getBoundingClientRect();
+    // let x = event.clientX;
     let x = event.clientX - dim.left;
     let y = event.clientY - dim.top;
+    // let y = event.clientY;
     return [x, y];
   };
 
   const addCircle = (event) => {
-    // get click coordinates
     let [x, y] = getClickCoords(event);
-
-    // make new svg circle element
-    // top-[${y}px] left-[${x}px]
-    // more info here: https://www.w3schools.com/graphics/svg_circle.asp
+    const screenWidth = window.innerWidth;
+    const screenHeight = screen.height;
+    const radius = (0.032552 * screenWidth) / 2;
     let newCircle = (
-      //   <circle cx={x} cy={y} r="20" stroke="black" strokeWidth="1" fill="red" />
       <div
-        className={`absolute w-[100px] h-[100px] rounded-full opacity-50 bg-red-600 z-10`}
-        style={{ top: y, left: x - 50 }}
+        className={`absolute rounded-full opacity-50 bg-red-600 z-10`}
+        // style={{ top: y + 25, left: x - 25 }}
+        style={{
+          width: 2 * radius,
+          height: 2 * radius,
+          top: y - 1.25 * radius + 0.07 * screenHeight,
+          //   top: 7380 * (screenWidth / 1920) + 0.07 * screenHeight - radius,
+          left: x - radius,
+          //   left: 1144 * (screenWidth / 1920) - radius,
+        }}
       ></div>
     );
-
-    // update the array of circles; you HAVE to spread the current array
-    // as 'circles' is immutible and will not accept new info
-    // let allCircles = [...circles, newCircle];
-
-    // update 'circles'
     setCircle(newCircle);
-    console.log(x, y);
+    // console.log(x, y);
+    return {
+      center: [x, y],
+      radius: 10,
+    };
   };
 
-  const handleClick = () => {
-    alert(checkIfContains(circle1, circle2));
+  const handleClick = (e) => {
+    const clickedCircle = addCircle(e);
+
+    console.log(clickedCircle);
+    const width = window.innerWidth;
+    console.log(width);
+    // alert(checkIfContains(circle1, circle2));
   };
 
   const circle1 = {
@@ -72,7 +107,14 @@ const MainContent = () => {
   return (
     <div className="w-full">
       {circle}
-      <img src={background2} onClick={addCircle} className="relative" />
+      <img
+        src={background2}
+        id="image"
+        onClick={(e) => handleClick(e)}
+        // onClick={(e) => getMouseCoords(e)}
+        className="relative"
+        style={{ width: '100vw', height: 'auto' }}
+      />
       {/* <svg width={'100%'} height={8000} onClick={addCircle}>
       </svg> */}
 
